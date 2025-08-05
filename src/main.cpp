2abeca0860e4a196b4f42baf726eaf0164d042d2
Colaboi2009 <alaa.mush@gmail.com>
2025-08-05 15:27:38 +0400

    collision works with ecs, added notes, changed back SDL_PollEvents

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@ SDL sdl;
 std::vector<SP<IElement>> ielements;
 std::vector<SP<ICollider>> icolliders;
 
-void handleInput(entt::registry& registry, const entt::entity& player) {
-    auto& velocity = registry.get<ecs::Velocity>(player);
+void handleInput(entt::registry &registry, const entt::entity &player) {
+    auto &velocity = registry.get<ecs::Velocity>(player);
 
     velocity.dx = 0;
     velocity.dy = 0;
@@ -36,7 +36,7 @@ void handleInput(entt::registry& registry, const entt::entity& player) {
     }
 }
 
-void handleMovementSystem(entt::registry& registry) {
+void handleMovementSystem(entt::registry &registry) {
     auto view = registry.view<ecs::Transform, ecs::Velocity>();
     for (auto [e, position, velocity] : view.each()) {
         position.box.x += velocity.dx;
@@ -44,47 +44,59 @@ void handleMovementSystem(entt::registry& registry) {
     }
 }
 
-void resolveCollision(entt::registry& registry, entt::entity& e1, entt::entity& e2) {
-    ecs::Transform& transformA = registry.get<ecs::Transform>(e1);
-    ecs::Transform& transformB = registry.get<ecs::Transform>(e2);
+void resolveCollision(entt::registry &registry, entt::entity &e1, entt::entity &e2) {
+    ecs::Transform &transL = registry.get<ecs::Transform>(e1);
+    ecs::Transform &transR = registry.get<ecs::Transform>(e2);
 
-    bool isAMovable = registry.any_of<ecs::Velocity>(e1);
-    bool isBMovable = registry.any_of<ecs::Velocity>(e2);
+	// there could be immovables with velocity, e.g. a moving platform (you're not really supposed to be able to move that yourself)
+    bool isLMovable = registry.any_of<ecs::Velocity>(e1);
+    bool isRMovable = registry.any_of<ecs::Velocity>(e2);
 
-    // TODO: Implement collisions
-	/*if (std::abs(r.x - lbotr.x) < std::abs(rbotr.y - l.y) && std::abs(r.x - lbotr.x) < std::abs(r.y - lbotr.y)) {
-		if (!coll->im_immovable && colr->im_immovable) {
-			coll->sex(r.x - l.w);
-		} else if (coll->im_immovable && !colr->im_immovable) {
-			colr->sex(l.x + l.w);
-		}
-	} else {
-		if (!coll->im_immovable && colr->im_immovable) {
-			if (std::abs(rbotr.y - l.y) < std::abs(r.y - lbotr.y)) {
-				coll->sey(rbotr.y);
-			} else {
-				coll->sey(r.y - l.h);
-			}
-		} else if (coll->im_immovable && !colr->im_immovable) {
-			if (std::abs(rbotr.y - l.y) < std::abs(r.y - lbotr.y)) {
-				colr->sey(l.y - r.h);
-			} else {
-				colr->sey(lbotr.y);
-			}
-		}
-	}*/
+    // TODO if needed in gameplay: Implement movable & movable (e.g. enemy collisions, pushing mechs), immovable & immovable (probably do
+    // nothing in this scenario, its likely map edge scenary overlaping) collisions
+
+    // it looks really ugly and messy to use transA and transB everywhere, so aliasing (temp comment)
+    SDL_FRect l = transL.box;
+    SDL_FRect r = transR.box;
+    SDL_FPoint lbotr = transL.botr();
+    SDL_FPoint rbotr = transR.botr();
+
+    if (std::abs(r.x - lbotr.x) < std::abs(rbotr.y - l.y) && std::abs(r.x - lbotr.x) < std::abs(r.y - lbotr.y)) {
+        if (isLMovable && !isRMovable) {
+            // keeping sets verbose (temp comment)
+            transL.box.x = r.x - l.w;
+        } else if (!isLMovable && isRMovable) {
+            transR.box.x = l.x + l.w;
+        }
+    } else {
+        if (isLMovable && !isRMovable) {
+            if (std::abs(rbotr.y - l.y) < std::abs(r.y - lbotr.y)) {
+                transL.box.y = rbotr.y;
+            } else {
+                transL.box.y = r.y - l.h;
+            }
+        } else if (!isLMovable && isRMovable) {
+            if (std::abs(rbotr.y - l.y) < std::abs(r.y - lbotr.y)) {
+                transR.box.y = l.y - r.h;
+            } else {
+                transR.box.y = lbotr.y;
+            }
+        }
+    }
 }
 
-void handleCollisionSystem(entt::registry& registry) {
+void handleCollisionSystem(entt::registry &registry) {
     auto view = registry.view<ecs::Transform, ecs::BoxCollider>();
     for (auto [e1, transformA] : view.each()) {
         for (auto [e2, transformB] : view.each()) {
-            if (e1 == e2) continue;
+            if (e1 == e2)
+                continue;
             if (rectsOverlap(transformA.box, transformB.box)) {
-                if (center(transformA.box).x > center(transformB.box).x) {
-					std::swap(e1, e2);
+                if (transformA.box.x + transformA.box.w / 2.f > transformB.box.x + transformB.box.w / 2.f) {
+                    resolveCollision(registry, e2, e1);
+                } else {
+                    resolveCollision(registry, e1, e2);
                 }
-                resolveCollision(registry, e1, e2);
             }
         }
     }
@@ -125,10 +137,9 @@ int main() {
         sdl.clear({40, 40, 40, 255});
 
         SDL_Event e;
-        if (SDL_PollEvent(&e)) {
+        while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
-                //goto EXIT;
-                break;
+                goto EXIT;
             }
         }
 
@@ -153,6 +164,6 @@ int main() {
         sdl.present();
     }
 
-//EXIT:
+EXIT:
     return 0;
 }
