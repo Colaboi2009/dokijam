69644bcca010d96125244504bedcb170e4914078
Colaboi2009 <alaa.mush@gmail.com>
2025-08-09 19:35:11 +0400

    center explosion around colliders if they exist

--- src/ecs/explosion.cpp
+++ src/ecs/explosion.cpp
@@ -3,28 +3,35 @@
 
 namespace ecs {
 
-static void triggerExplosion(entt::registry &reg, entt::entity e) {
-    if (reg.any_of<Explosion>(e) && !reg.any_of<JustDieAfter>(e)) {
-        // CHAIN REACTION :D
-        // delay by 100 milliseconds to make it *DRAMATIC*
-        reg.emplace<JustDieAfter>(e, 100);
+static SDL_FPoint getColliderCenter(entt::entity e, Position originalPos, auto &boxView, auto &circleView) {
+    SDL_FPoint center = {originalPos.x, originalPos.y}; // make it center of the collider if it exists
+    if (boxView.contains(e)) {
+        auto [_, box] = boxView.get(e);
+        center.x += box.xOffset + box.width / 2.f;
+        center.y += box.yOffset + box.height / 2.f;
+    } else if (circleView.contains(e)) {
+        auto [_, cir] = circleView.get(e);
+        center.x += cir.xOffset;
+        center.y += cir.yOffset;
     }
+    return center;
 }
 
 void explosion(entt::registry &reg, Animation explosionAnimation) {
     std::vector<entt::entity> to_explode;
 
     auto view = reg.view<Position, Explosion>();
-	auto boxColliders = reg.view<Position, BoxCollider>();
-	auto circleColliders = reg.view<Position, CircleCollider>();
+    auto boxColliders = reg.view<Position, BoxCollider>();
+    auto circleColliders = reg.view<Position, CircleCollider>();
     for (auto [e, pos, exp] : view.each()) {
         if (exp.shouldTrigger) {
+            SDL_FPoint explosion_center = getColliderCenter(e, pos, boxColliders, circleColliders);
             for (auto [entity_box, pos_box, col_box] : boxColliders.each()) {
                 if (e == entity_box) {
                     continue;
                 }
                 const SDL_FRect rect = {pos_box.x + col_box.xOffset, pos_box.y + col_box.yOffset, col_box.width, col_box.height};
-                if (circleInFRect({pos.x, pos.y}, exp.radius, rect)) {
+                if (circleInFRect(explosion_center, exp.radius, rect)) {
                     to_explode.push_back(entity_box);
                 }
             }
@@ -34,23 +41,29 @@ void explosion(entt::registry &reg, Animation explosionAnimation) {
                     continue;
                 }
                 const SDL_FPoint center = {pos.x + col_cir.xOffset, pos.y + col_cir.yOffset};
-                if (circlesOverlap({pos.x, pos.y}, exp.radius, center, col_cir.radius)) {
+                if (circlesOverlap(explosion_center, exp.radius, center, col_cir.radius)) {
                     to_explode.push_back(entity_cir);
                 }
             }
         }
     }
     for (auto e : to_explode) {
-        triggerExplosion(reg, e);
+        if (reg.any_of<Explosion>(e) && !reg.any_of<JustDieAfter>(e)) {
+            // CHAIN REACTION :D
+            // delay by 100 milliseconds to make it *DRAMATIC*
+            reg.emplace<JustDieAfter>(e, 100);
+        }
     }
     for (auto [e, pos, exp] : view.each()) {
         if (exp.shouldTrigger) {
-			entt::entity exp = reg.create();
-			reg.emplace<Position>(exp, pos.x, pos.y);
-			SP<Animation> anim = std::make_shared<Animation>(explosionAnimation);
-			anim->play();
-			reg.emplace<Sprite>(exp, anim, 5);
-			reg.emplace<JustDieAfter>(exp, 1000);
+            SDL_FPoint animation_center = getColliderCenter(e, pos, boxColliders, circleColliders);
+
+            entt::entity exp = reg.create();
+            reg.emplace<Position>(exp, animation_center.x, animation_center.y);
+            SP<Animation> anim = std::make_shared<Animation>(explosionAnimation);
+            anim->play();
+            reg.emplace<Sprite>(exp, anim, 5);
+            reg.emplace<JustDieAfter>(exp, 1000);
 
             reg.destroy(e);
         }
