ff606d9715b486212a1461505625551ecf0ea107
Skulaurun Mrusal <Skulaurun@seznam.cz>
2025-08-17 14:49:13 +0200

    Edit src/main.cpp to run in Emscripten

--- src/main.cpp
+++ src/main.cpp
@@ -9,138 +9,185 @@
 #include "ui.hpp"
 #include <entt/entt.hpp>
 
+#include <emscripten.h>
+
+/*
+	note: Edited by Skulaurun for Emscripten,
+	This is spaghetti anyway, so quality is not a concern.
+
+	The 'restart' works only sometimes, as resource loading is tied
+	to game object initialization......
+*/
+
 SDL sdl;
 Audio audio;
 
-int main() {
-	srand(time(0));
-    entt::registry registry;
+void init();
+
+struct Game {
+	entt::registry registry;
+	entt::entity player;
+	ecs::Player* playerComponent;
+
+	SP<Animation> animation;
+	SP<Animation> explosionAnimation;
+
+	std::chrono::steady_clock::time_point BEGINTIME;
+
+	bool restart = false;
+	bool gameWon = false;
+	bool showingQuestionScreen = false;
+};
+
+Game* game;
+bool running = true;
+uint64_t lastTime;
+
+static void mainLoop() {
+	static TTF_Font* font = TTF_OpenFont("art/sourcecodevf/sourcecodevf.ttf", 64.f);
+
+	if (!running) {
+		#ifdef __EMSCRIPTEN__
+			emscripten_cancel_main_loop();
+		#endif
+	}
+
+	if (game->restart) {
+		init();
+	}
+
+	uint64_t now = SDL_GetPerformanceCounter();
+	double deltaTime = static_cast<double>(now - lastTime) * 1000.0 / SDL_GetPerformanceFrequency();
+	lastTime = now;
+
+	sdl.clear({40, 40, 40, 255});
+
+	SDL_Event e;
+	while (SDL_PollEvent(&e)) {
+		if (e.type == SDL_EVENT_QUIT) {
+			running = false;
+		}
+		if (game->playerComponent->alive) {
+			ecs::syncInput(game->registry, game->player, e);
+		}
+		if (e.type == SDL_EVENT_KEY_DOWN) {
+			if (e.key.key == SDLK_R) {
+				game->restart = true;
+			} else if (e.key.key == SDLK_W && !game->playerComponent->alive) {
+				game->showingQuestionScreen = !game->showingQuestionScreen;
+			}
+		}
+	}
+
+	float timeTaken = 0;
+	if (game->playerComponent->alive) {
+		ecs::asyncInput(game->registry, game->player);
+		ecs::heldDragoonMover(game->registry, game->player);
+		ecs::spawnCrystals(game->registry, 2, deltaTime);
+		ecs::moveCrystals(game->registry);
+		ecs::murderCrystals(game->registry);
+		ecs::handleCrystalSheilds(game->registry, deltaTime);
+		ecs::handleRiverDivergence(game->registry);
+
+		ecs::collision(game->registry, deltaTime);
+		if (ecs::boss(game->registry, deltaTime, game->player)) {
+			game->playerComponent->alive = false;
+			game->gameWon = true;
+		}
+		ecs::movement(game->registry, deltaTime);
+		ecs::explosion(game->registry, *game->explosionAnimation, audio, 4);
+		ecs::spawn(game->registry);
+
+		ecs::forcePlayerWithinBounds(game->registry, game->player);
+		ecs::cleanup(game->registry, deltaTime);
+		ecs::render(game->registry, sdl);
+		timeTaken = endTimer(game->BEGINTIME);
+		RawText::Render(std::format("{}", (int)timeTaken), {7, 10}, true, {255, 255, 255, 255}, font, .35f);
+	} else {
+		if (game->gameWon) {
+			showWonScreen(font, timeTaken);
+		} else {
+			if (game->showingQuestionScreen) {
+				showQuestionScreen(font);
+			} else {
+				showDeathScreen(font);
+			}
+		}
+	}
+	
+	sdl.present();
+}
+
+void init() {
+	delete game;
+	game = new Game();
 
 	for (int i = 0; i < 10; i++) { // im lazy, just queue it 10 times
 		audio.queueMusic("art/doki_music_caveofancient.wav");
 	}
 
-	TTF_Font *font = TTF_OpenFont("art/sourcecodevf/sourcecodevf.ttf", 64.f);
+    game->animation = std::make_shared<Animation>("doki.aseprite");
+	game->animation->repeat(0);
+	game->explosionAnimation = std::make_shared<Animation>("explosion_real.gif");
 
-    SP<Animation> animation = std::make_shared<Animation>("doki.aseprite");
-	animation->repeat(0);
+    //game->tileMap = std::make_shared<TileMap>("tilemap.aseprite");
+    //game->tileMap->setLevel("default");
 
-	Animation explosionAnimation{"explosion_real.gif"};
+    game->player = game->registry.create();
+    game->registry.emplace<ecs::Position>(game->player, 0, -400);
+    game->registry.emplace<ecs::Velocity>(game->player);
+    game->registry.emplace<ecs::BoxCollider>(game->player, -20, -35, 30, 80);
+    game->registry.emplace<ecs::Sprite>(game->player, game->animation, .6f);
+    game->registry.emplace<ecs::Spawner>(game->player, ecs::Spawner::Type::Dragoon);
+	game->registry.emplace<ecs::Camera>(game->player);
+	game->registry.emplace<ecs::DragoonHolder>(game->player);
+	game->playerComponent = &game->registry.emplace<ecs::Player>(game->player);
 
-    SP<TileMap> tileMap = std::make_shared<TileMap>("tilemap.aseprite");
-    tileMap->setLevel("default");
+	ecs::riversInit(game->registry);
+	ecs::bossInit(game->registry, game->player);
 
-    const entt::entity player = registry.create();
-    registry.emplace<ecs::Position>(player, 0, -400);
-    registry.emplace<ecs::Velocity>(player);
-    registry.emplace<ecs::BoxCollider>(player, -20, -35, 30, 80);
-    registry.emplace<ecs::Sprite>(player, animation, .6f);
-    registry.emplace<ecs::Spawner>(player, ecs::Spawner::Type::Dragoon);
-	registry.emplace<ecs::Camera>(player);
-	registry.emplace<ecs::DragoonHolder>(player);
-	ecs::Player &playerComponent = registry.emplace<ecs::Player>(player);
-
-	ecs::riversInit(registry);
-	ecs::bossInit(registry, player);
-
-	const entt::entity floor = registry.create();
-	registry.emplace<ecs::Position>(floor, -1250, -700, 1.f);
-	registry.emplace<ecs::Rectangle>(floor, 2500, 1400, SDL_Color{90, 30, 30, 255});
+	const entt::entity floor = game->registry.create();
+	game->registry.emplace<ecs::Position>(floor, -1250, -700, 1.f);
+	game->registry.emplace<ecs::Rectangle>(floor, 2500, 1400, SDL_Color{90, 30, 30, 255});
 
     // Register TileMap Level-Collider-Entities (never used lmao)
-    auto view = registry.view<ecs::Position, ecs::TileMapSprite>();
+    auto view = game->registry.view<ecs::Position, ecs::TileMapSprite>();
     for (const auto& [e, position, tileMapSprite] : view.each()) {
         tileMapSprite.tilemap->registerEntities(
-            registry, {position.x, position.y},
+            game->registry, {position.x, position.y},
             tileMapSprite.scale
         );
     }
 
-	bool restart = false;
-	bool gameWon = false;
-	bool showingQuestionScreen = false;
+	lastTime = SDL_GetPerformanceCounter();
+	game->BEGINTIME = beginTimer();
+}
 
-    bool running = true;
-    uint64_t lastTime = SDL_GetPerformanceCounter();
-	auto BEGINTIME = beginTimer();
-	float timeTaken = 0;
-    while (running) {
-        uint64_t now = SDL_GetPerformanceCounter();
-        double deltaTime = static_cast<double>(now - lastTime) * 1000.0 / SDL_GetPerformanceFrequency();
-        lastTime = now;
+int main() {
+	srand(time(0));
 
-        sdl.clear({40, 40, 40, 255});
-
-        SDL_Event e;
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-			if (playerComponent.alive) {
-				ecs::syncInput(registry, player, e);
-			}
-			if (e.type == SDL_EVENT_KEY_DOWN) {
-				if (e.key.key == SDLK_R) {
-					restart = true;
-					running = false;
-				} else if (e.key.key == SDLK_W && !playerComponent.alive) {
-					showingQuestionScreen = !showingQuestionScreen;
-				}
-			}
-        }
-
-		if (playerComponent.alive) {
-			ecs::asyncInput(registry, player);
-			ecs::heldDragoonMover(registry, player);
-			ecs::spawnCrystals(registry, 2, deltaTime);
-			ecs::moveCrystals(registry);
-			ecs::murderCrystals(registry);
-			ecs::handleCrystalSheilds(registry, deltaTime);
-			ecs::handleRiverDivergence(registry);
-
-			ecs::collision(registry, deltaTime);
-			if (ecs::boss(registry, deltaTime, player)) {
-				playerComponent.alive = false;
-				gameWon = true;
-			}
-			ecs::movement(registry, deltaTime);
-			ecs::explosion(registry, explosionAnimation, audio, 4);
-			ecs::spawn(registry);
-
-			ecs::forcePlayerWithinBounds(registry, player);
-			ecs::cleanup(registry, deltaTime);
-			ecs::render(registry, sdl);
-			timeTaken = endTimer(BEGINTIME);
-			RawText::Render(std::format("{}", (int)timeTaken), {7, 10}, true, {255, 255, 255, 255}, font, .35f);
-		} else {
-			if (gameWon) {
-				showWonScreen(font, timeTaken);
-			} else {
-				if (showingQuestionScreen) {
-					showQuestionScreen(font);
-				} else {
-					showDeathScreen(font);
-				}
-			}
+	init();
+	#ifdef __EMSCRIPTEN__
+		emscripten_set_main_loop(mainLoop, 0, 1);
+	#else
+		while (running) {
+			mainLoop();
 		}
+	#endif
 
-        
-        sdl.present();
-    }
-
-	if (restart) {
-		main(); // LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO im lazy
-		/*
-		 * listen, in my defense, its BASICALLY equivalent to making an init function and then exporting the game state, but you see
-		 * im lazy
-		 * i actually did do that
-		 * then I thought back and said
-		 * "hmm, i think it would be more fitting if this was a recursive main.
-		 * This way, its a CHAIN REACTION." (famous last words)
-		 * dont send a hitman after me
-		 * this is funny as shit and you KNOW it
-		*/
-	}
+	//if (restart) {
+	//	main(); // LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO im lazy
+	//	/*
+	//	 * listen, in my defense, its BASICALLY equivalent to making an init function and then exporting the game state, but you see
+	//	 * im lazy
+	//	 * i actually did do that
+	//	 * then I thought back and said
+	//	 * "hmm, i think it would be more fitting if this was a recursive main.
+	//	 * This way, its a CHAIN REACTION." (famous last words)
+	//	 * dont send a hitman after me
+	//	 * this is funny as shit and you KNOW it
+	//	*/
+	//}
 
     return 0;
 }
